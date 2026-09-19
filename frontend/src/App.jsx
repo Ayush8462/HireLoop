6082d9dd1e7f5a5346e9f2455f1ad25947ee5110
Ayush Das <ayushdas7354@gmail.com>
2026-09-19 15:06:28 +0530

    added new features

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
+import { Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage"
-
+import Signup from "./pages/auth/signup"
+import Login from "./pages/auth/login"
+import Dashboard from "./pages/dashboard/dashboard"
 function App() {
 
   return (
     <>
-      <LandingPage />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </>
   )
 }
