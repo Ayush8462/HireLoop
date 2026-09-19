b8eca233d66d8ad65edfea145a8e59bd09822b05
Ayush Das <ayushdas7354@gmail.com>
2026-09-19 15:25:11 +0530

    chore: ignore files

--- services/auth-service/dist/middlewares/rate-limit.middlware.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=rate-limit.middlware.js.map
--- services/auth-service/dist/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { login, register, refresh, logout } from "../controllers/auth.controller.js";
-const router = Router();
-router.post("/register", register);
-router.post("/login", login);
-router.post("/refresh", refresh);
-router.post("/logout", logout);
-export default router;
-//# sourceMappingURL=auth.routes.js.map
