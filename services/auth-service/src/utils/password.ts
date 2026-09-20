70cb528e020bf5feaa731ae7bd8751bbac97a8f8
Ayush Das <ayushdas7354@gmail.com>
2026-09-20 19:43:28 +0530

    build docker and improved docker compose

--- services/auth-service/src/utils/password.ts
+++ services/auth-service/src/utils/password.ts
@@ -1,16 +1,16 @@
-import argon2 from "argon2";
+import bcrypt from "bcrypt";
+
+const SALT_ROUNDS = 10;
 
 export async function hashPassword(
     password: string
-):Promise<string>{
-    return argon2.hash(password,{
-        type:argon2.argon2id,
-    })
+): Promise<string> {
+    return bcrypt.hash(password, SALT_ROUNDS);
 }
 
 export async function verifyPassword(
     password: string,
     passwordHash: string
-): Promise<boolean>{
-    return argon2.verify(passwordHash,password);
+): Promise<boolean> {
+    return bcrypt.compare(password, passwordHash);
 }
